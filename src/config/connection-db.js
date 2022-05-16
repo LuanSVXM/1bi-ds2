6bd1e191d10be2d58e3b589bd54c61594ac4e800
Luan Vieira <36699408+LuanSVXM@users.noreply.github.com>
2022-05-16 10:54:36 -0300

    Add files via upload

---
+++ src/config/connection-db.js
@@ -0,0 +1,21 @@
+const { Client } = require('pg')
+
+const dbcon = new Client({
+    connectionString: '',
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+dbcon.connect(err => {
+    if (err) {
+        console.log("ERRO!!! NAO FOI POSSIVEL CONECTAR NO BANCO");
+        console.log( { err });
+    } else {
+        console.log("BANCO CONECTADO COM SUCESSO");
+    }
+});
+
+module.exports = {
+    dbcon
+}
